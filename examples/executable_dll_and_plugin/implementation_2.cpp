795472a3e0ff74b2d511fa1f6f851d9692c035b3
Kyle-11 <571133@students.wits.ac.za>
2017-09-26 08:19:59 +0200

    fvvh

--- examples/executable_dll_and_plugin/implementation_2.cpp
+++
@@ -1,8 +0,0 @@
-// note that DOCTEST_CONFIG_IMPLEMENTATION_IN_DLL should not be defined here
-#include "doctest.h"
-
-#include <cstdio>
-
-TEST_CASE("implementation_2") {
-    printf("I am a test from the implementation_2!\n");
-}
--- scripts/playground/test.cpp
+++
@@ -1,4 +0,0 @@
-#include "parts/doctest_fwd.h"
-
-#include <iostream>
-using namespace std;
